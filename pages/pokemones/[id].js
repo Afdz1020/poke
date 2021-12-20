6e9528d77e9830309573602077a95afd33b0237a
Andrés Felipe Díaz Zapata <adiaz@pratech.local>
2021-12-20 12:51:16 -0500

    commit inicial

---
+++ pages/pokemones/[id].js
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Pokemon = ({ data }) => {
+  const router = useRouter();
+  console.log(router);
+
+  if (router.isFallback) {
+    return <p>Cargando...</p>;
+  }
+  return (
+    <div>
+      <h1>
+        {data.name} número #{data.id}
+      </h1>
+      <Image src={`${data.sprites.front_default}`} width={400} height={400} />
+      {/* <Image src={data.sprites.front_default} width={400} height={400} /> */}
+      <Link href="/">Volver</Link>
+    </div>
+  );
+};
+
+export default Pokemon;
+
+export const getStaticProps = async ({ params }) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${params.id}`
+  );
+  const data = await response.json();
+  return {
+    props: { data },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = [{ params: { id: '1' } }, { params: { id: '2' } }];
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+// export const getServerSideProps = async ({ params }) => {
+//   const response = await fetch(
+//     `https://pokeapi.co/api/v2/pokemon/${params.id}`
+//   );
+//   const data = await response.json();
+//   return {
+//     props: { data },
+//   };
+// };
